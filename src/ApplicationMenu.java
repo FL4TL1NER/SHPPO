23f86feba424b5baa8c8a336c372a742f0c35654
FL4TL1NER <major_fl4tl1ner@outlook.com>
2024-06-02 14:37:05 +0300

    Application menu refactored

--- src/ApplicationMenu.java
+++ src/ApplicationMenu.java
@@ -1,145 +1,181 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ApplicationMenu {
-    public static void StartApplicationMenu(GlobalVariables globalVariables, FiniteStateMachine fsm)
+    private FiniteStateMachine fsm;
+    private GlobalVariables globalVariables;
+    private Scanner in;
+    private String input;
+    private boolean isRunning;
+    @FunctionalInterface
+    interface Procedure{
+        void func();
+    }
+    Map<String,Procedure> menuMap = new HashMap<>();
+    public ApplicationMenu(GlobalVariables globalVariables, FiniteStateMachine fsm)
     {
-        Scanner in = new Scanner(System.in);
-        boolean isRunning = true;
+        this.fsm = fsm;
+        this.globalVariables = globalVariables;
+        this.in = new Scanner(System.in);
+        menuMap.put("create_state", this::createState);
+        menuMap.put("create_gvar", this::createGvar);
+        menuMap.put("create_rule", this::createRule);
+        menuMap.put("change_gvar", this::changeGvar);
+        menuMap.put("delete_state", this::deleteState);
+        menuMap.put("delete_gvar", this::deleteGvar);
+        menuMap.put("delete_rule", this::deleteRule);
+        menuMap.put("get_active_state", this::getActiveState);
+        menuMap.put("get_gvar", this::getGvar);
+        menuMap.put("get_rules", this::getRules);
+        menuMap.put("exit", this::exit);
+    }
+    public void StartApplicationMenu()
+    {
+        isRunning = true;
         while(isRunning)
         {
             System.out.print("input command: ");
-            String input = in.nextLine();
-            // state manipulation logic
-            if(input.equals("create_state"))
+            input = in.nextLine();
+
+            if(menuMap.containsKey(input))
             {
-                StateBuilder stateBuilder = new StateBuilder();
-
-                System.out.print("enter name: ");
-                stateBuilder.name = in.nextLine();
-
-                int c;
-                System.out.print("enter n of job: ");
-                c = Integer.parseInt(in.nextLine());
-                switch (c)
-                {
-                    case 1:
-                        stateBuilder.job = new Runnable1();
-                        break;
-                    case 2:
-                        stateBuilder.job = new Runnable2();
-                        break;
-                    default:
-                        stateBuilder.job = new Runnable3();
-                        break;
-                }
-
-                fsm.addState(stateBuilder.build());
-            }
-            if(input.equals("create_gvar"))
-            {
-                System.out.print("enter name: ");
-                String name = in.nextLine();
-
-                System.out.print("enter data: ");
-                String data = in.nextLine();
-
-                globalVariables.addGVar(name,data);
-            }
-            if(input.equals("create_rule"))
-            {
-                System.out.print("enter name of state: ");
-                String stateName = in.nextLine();
-
-                StateRuleBuilder stateRuleBuilder = new StateRuleBuilder();
-
-                System.out.print("enter name of next state: ");
-                stateRuleBuilder.newStateName = in.nextLine();
-
-                System.out.print("enter type of event[gvar/end]: ");
-                input = in.nextLine();
-                Event event;
-                if(input.equals("end"))
-                {
-                    int c;
-                    System.out.print("enter n of job: ");
-                    c = Integer.parseInt(in.nextLine());
-                    event = switch (c) {
-                        case 1 -> new EventEndAlg(1);
-                        case 2 -> new EventEndAlg(2);
-                        default -> new EventEndAlg(3);
-                    };
-                }
-                else
-                {
-                    System.out.print("enter name of gvar: ");
-                    String name = in.nextLine();
-                    System.out.print("enter data: ");
-                    String data = in.nextLine();
-
-                    event = new EventGVarChange(name,data);
-                }
-
-                stateRuleBuilder.changeCondition = event;
-
-                fsm.addRule(stateName,stateRuleBuilder.build());
-            }
-            // global variables logic
-            if(input.equals("change_gvar"))
-            {
-                System.out.print("enter name of gvar: ");
-                String name = in.nextLine();
-                System.out.print("enter data: ");
-                String data = in.nextLine();
-
-                globalVariables.setGVar(name,data);
-            }
-
-            if(input.equals("delete_state"))
-            {
-                System.out.print("enter name of state: ");
-                String name = in.nextLine();
-
-                fsm.removeState(name);
-            }
-            if(input.equals("delete_gvar"))
-            {
-                System.out.print("enter name of gvar: ");
-                String name = in.nextLine();
-
-                globalVariables.deleteGVar(name);
-            }
-            if(input.equals("delete_rule"))
-            {
-                System.out.print("enter name of rule: ");
-                String name = in.nextLine();
-                System.out.print("enter second name of rule: ");
-                String name2 = in.nextLine();
-
-                fsm.removeRule(name,name2);
-            }
-            // state logic
-            if(input.equals("get_active_state"))
-            {
-                System.out.println(fsm.getActiveState());
-            }
-
-            if(input.equals("get_gvar"))
-            {
-                System.out.print("enter name of gvar: ");
-                String name = in.nextLine();
-
-                System.out.println(globalVariables.getDataGVar(name));
-            }
-
-            if(input.equals("get_rules"))
-            {
-                System.out.println("enter name of state: ");
-            }
-
-            if(input.equals("exit"))
-            {
-                isRunning = false;
+                menuMap.get(input).func();
             }
         }
+    }
+
+    private void exit()
+    {
+        isRunning = false;
+    }
+    private void createState()
+    {
+        StateBuilder stateBuilder = new StateBuilder();
+
+        System.out.print("enter name: ");
+        stateBuilder.name = in.nextLine();
+
+        int c;
+        System.out.print("enter n of job: ");
+        c = Integer.parseInt(in.nextLine());
+        switch (c)
+        {
+            case 1:
+                stateBuilder.job = new Runnable1();
+                break;
+            case 2:
+                stateBuilder.job = new Runnable2();
+                break;
+            default:
+                stateBuilder.job = new Runnable3();
+                break;
+        }
+
+        fsm.addState(stateBuilder.build());
+    }
+
+    private void createRule()
+    {
+        System.out.print("enter name of state: ");
+        String stateName = in.nextLine();
+
+        StateRuleBuilder stateRuleBuilder = new StateRuleBuilder();
+
+        System.out.print("enter name of next state: ");
+        stateRuleBuilder.newStateName = in.nextLine();
+
+        System.out.print("enter type of event[gvar/end]: ");
+        input = in.nextLine();
+        Event event;
+        if(input.equals("end"))
+        {
+            int c;
+            System.out.print("enter n of job: ");
+            c = Integer.parseInt(in.nextLine());
+            event = switch (c) {
+                case 1 -> new EventEndAlg(1);
+                case 2 -> new EventEndAlg(2);
+                default -> new EventEndAlg(3);
+            };
+        }
+        else
+        {
+            System.out.print("enter name of gvar: ");
+            String name = in.nextLine();
+            System.out.print("enter data: ");
+            String data = in.nextLine();
+
+            event = new EventGVarChange(name,data);
+        }
+
+        stateRuleBuilder.changeCondition = event;
+
+        fsm.addRule(stateName,stateRuleBuilder.build());
+    }
+
+    private void createGvar()
+    {
+        System.out.print("enter name: ");
+        String name = in.nextLine();
+
+        System.out.print("enter data: ");
+        String data = in.nextLine();
+
+        globalVariables.addGVar(name,data);
+    }
+
+    private void changeGvar()
+    {
+        System.out.print("enter name of gvar: ");
+        String name = in.nextLine();
+        System.out.print("enter data: ");
+        String data = in.nextLine();
+
+        globalVariables.setGVar(name,data);
+    }
+
+    private void deleteState()
+    {
+        System.out.print("enter name of state: ");
+        String name = in.nextLine();
+
+        fsm.removeState(name);
+    }
+
+    private void deleteGvar()
+    {
+        System.out.print("enter name of gvar: ");
+        String name = in.nextLine();
+
+        globalVariables.deleteGVar(name);
+    }
+
+    private void deleteRule()
+    {
+        System.out.print("enter name of rule: ");
+        String name = in.nextLine();
+        System.out.print("enter second name of rule: ");
+        String name2 = in.nextLine();
+
+        fsm.removeRule(name,name2);
+    }
+
+    private void getActiveState()
+    {
+        System.out.println(fsm.getActiveState());
+    }
+
+    private void getGvar()
+    {
+        System.out.print("enter name of gvar: ");
+        String name = in.nextLine();
+
+        System.out.println(globalVariables.getDataGVar(name));
+    }
+
+    private void getRules()
+    {
+        System.out.println("enter name of state: ");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScanBeanConfig.class);
+
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
 
         GlobalVariables globalVariables = context.getBean(GlobalVariables.class);
         FiniteStateMachine finiteStateMachine = context.getBean(FiniteStateMachine.class);
@@ -14,20 +18,23 @@ public class Main {
         finiteStateMachine.subscribe(threadPool);
         threadPool.subscribe(finiteStateMachine);
 
-        globalVariables.addGVar("water","false");
-        globalVariables.addGVar("jump","false");
+        globalVariables.addGVar("water", "false");
+        globalVariables.addGVar("jump", "false");
 
-        finiteStateMachine.addState(context.getBean("stand",State.class));
-        finiteStateMachine.addState(context.getBean("air",State.class));
-        finiteStateMachine.addState(context.getBean("swim",State.class));
+        finiteStateMachine.addState(context.getBean("stand", State.class));
+        finiteStateMachine.addState(context.getBean("air", State.class));
+        finiteStateMachine.addState(context.getBean("swim", State.class));
 
-        finiteStateMachine.addRule("stand",context.getBean("toswim",StateRule.class));
-        finiteStateMachine.addRule("swim",context.getBean("fromswim",StateRule.class));
-        finiteStateMachine.addRule("stand",context.getBean("toair",StateRule.class));
-        finiteStateMachine.addRule("air",context.getBean("fromair",StateRule.class));
+        finiteStateMachine.addRule("stand", context.getBean("toswim", StateRule.class));
+        finiteStateMachine.addRule("swim", context.getBean("fromswim", StateRule.class));
+        finiteStateMachine.addRule("stand", context.getBean("toair", StateRule.class));
+        finiteStateMachine.addRule("air", context.getBean("fromair", StateRule.class));
+
+        LoggingAspect a = context.getBean(LoggingAspect.class);
 
         context.close();
 
-        ApplicationMenu.StartApplicationMenu(globalVariables, finiteStateMachine);
+        ApplicationMenu applicationMenu = new ApplicationMenu(globalVariables, finiteStateMachine);
+        applicationMenu.StartApplicationMenu();
     }
 }
